0bf6be5bab1ac915195f9926c49854526f643f0c
unknown <leixiaohua1020@126.com>
2015-02-12 15:38:06 +0800

    fix

--- simplest_ffmpeg_mem_player/compile_gcc.sh
+++ simplest_ffmpeg_mem_player/compile_gcc.sh
@@ -10,5 +10,5 @@
 #
 #compile
 gcc simplest_ffmpeg_mem_player.cpp -g -o simplest_ffmpeg_mem_player.out -lstdc++ \
--I /usr/local/include -L /usr/local/lib -framework Cocoa -lSDLmain -lSDL -lavformat -lavcodec -lavutil -lswscale
+-I /usr/local/include -L /usr/local/lib -lSDLmain -lSDL -lavformat -lavcodec -lavutil -lswscale
 
